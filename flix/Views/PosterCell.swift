d83f2e5cd9407e6bee489d7444dfcb4c5572d70b
Christopher Guan <cwguan@ucsd.edu>
2018-02-06 17:11:49 -0800

    Implemented collection View

---
+++ flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix
+//
+//  Created by Christopher Guan on 2/6/18.
+//  Copyright © 2018 Christopher Guan. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
