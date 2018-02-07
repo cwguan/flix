6eef126e2c423003379c9ba06571cfa7432254fd
Christopher Guan <cwguan@ucsd.edu>
2018-02-06 16:25:40 -0800

    Finished detail view

--- flix/ViewControllers/DetailViewController.swift
+++ flix/ViewControllers/DetailViewController.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
+enum MovieKeys {
+    static let title = "title"
+    static let backdropPath = "backdrop_path"
+    static let posterPath = "poster_path"
+}
+
 class DetailViewController: UIViewController {
 
     @IBOutlet weak var backDropImageView: UIImageView!
@@ -24,7 +30,19 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
 
         if let movie = movie {
+            titleLabel.text = movie[MovieKeys.title] as? String
+            releaseDateLabel.text = movie["release_date"] as? String
+            overviewLabel.text = movie["overview"] as? String
             
+            let backdropPathString = movie[MovieKeys.backdropPath] as! String
+            let posterPathString = movie[MovieKeys.posterPath] as! String
+            let baseURLString = "https://image.tmdb.org/t/p/w500"
+            
+            let backdropURL = URL(string: baseURLString + backdropPathString)!
+            backDropImageView.af_setImage(withURL: backdropURL)
+            
+            let posterPathURL = URL(string: baseURLString + posterPathString)!
+            posterImageView.af_setImage(withURL: posterPathURL)
         }
     }
 
